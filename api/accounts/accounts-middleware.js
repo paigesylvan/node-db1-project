9fe19663992b53c3853a4256a88dc7846fe534cc
PaigeSylvan <paigesylvan@gmail.com>
2024-01-06 15:48:29 -0600

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = { status: 400 }
@@ -21,9 +22,20 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+      const existing = await db('accounts')
+      .where('name', req.body.name.trim())
+      .first()
+
+      if (existing) {
+        next({ status: 400, message: 'that name is taken'})
+      } else {
+        next()
+      }
+  } catch (err) {
+      next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
